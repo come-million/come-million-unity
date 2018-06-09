a9e1e6dc7329d7a8c6b7b0ab6962dc3b993c6b1c
Eran <eranws@gmail.com>
2018-06-09 12:40:38 -0400

    Tile Animations (#1)
    * add model (fbx) of tile, scene, prefabs
    
    * add LedBurnColorSender, and example scene
    
    * indent
    
    * tabs vs. spaces

---
+++ Assets/Scripts/TileBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBehaviour : MonoBehaviour {
+
+    // Use this for initialization
+    // setting up tile position, angle, connectivity
+    void Start () {
+       foreach (Transform child in transform) {
+            child.position += Vector3.up * (child.position.y + child.position.x / 2.0f); // example
+        } 		
+    }
+
+    // Update is called once per frame
+    // color and animations go here
+    void Update () {
+        foreach (Transform child in transform) {
+            child.GetComponent<Renderer>().material.color = new Color32(1, 234, 32, 128); // example
+        }
+    }
+}
