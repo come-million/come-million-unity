9cab48a90aab52cf4753057407db4761f80d956c
Amir Blum <amirgiraffe@gmail.com>
2018-06-10 00:10:45 +0300

    make tile object from triangles

--- Assets/Scripts/TileBehaviour.cs
+++ Assets/Scripts/TileBehaviour.cs
@@ -4,12 +4,43 @@ using UnityEngine;
 
 public class TileBehaviour : MonoBehaviour {
 
+    const int TRI_IN_TILE = 50;
+    const int TRI_IN_ROW = 5;
+
+    public GameObject tri_prefab;
+
+    public GameObject[] triangles = new GameObject[TRI_IN_TILE];
+
 	// Use this for initialization
 	// setting up tile position, angle, connectivity
 	void Start () {
-       foreach (Transform child in transform) {
-            child.position += Vector3.up * (child.position.y + child.position.x / 2.0f); // example
-        } 		
+        /*Quaternion triLookUp = Quaternion.Euler(new Vector3(90, 0, 0));
+        Quaternion triLookDown = Quaternion.Euler(new Vector3(270, 0, 0));
+        for (int i=0; i< TRI_IN_TILE; i++)
+        {
+            int rowInt = (i / TRI_IN_ROW);
+            float row = (float)rowInt;
+            bool isUp = i % (2 * TRI_IN_ROW) < TRI_IN_ROW;
+            float col = (float)i % TRI_IN_ROW;
+            if(isUp)
+            {
+                col = (TRI_IN_ROW - 1 - col) + 0.5f;
+                row = row - 0.5f;
+            }
+            if(rowInt % 4 == 2)
+            {
+                col = col - 0.5f;
+            }
+            if (rowInt % 4 == 3)
+            {
+                col = col + 0.5f;
+            }
+            Quaternion triQuaternion = isUp ? triLookUp : triLookDown;
+            GameObject prefab = (GameObject)Resources.Load("Triangle");
+            triangles[i] = (GameObject) Instantiate(prefab, new Vector3(col / 9.0f, row / 17.0f, isUp ? 0.0f : 0.05f), triQuaternion);
+            triangles[i].transform.parent = this.transform;
+            triangles[i].name = "Tri" + i;
+        }*/
 	}
 
 	// Update is called once per frame
