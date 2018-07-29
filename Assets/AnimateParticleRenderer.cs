8b539609ff8d9c5f21db7be1e5bcb73600276a8d
yossilan <yossi@currentcircus.com>
2018-07-29 20:03:40 +0300

    Yossi: A new scene for standalone build setup
    ...

---
+++ Assets/AnimateParticleRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateParticleRenderer : MonoBehaviour {
+
+    public Renderer rend;
+    public float baseAlpha = 0.35f;
+    public float alphaRange = 0.2f;
+
+	void Start ()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update ()
+    {
+        ////Set the main Color of the Material to green
+        //rend.material.shader = Shader.Find("_TintColor");
+        Color currentColor = rend.material.GetColor("_TintColor");
+
+        currentColor.a = baseAlpha + alphaRange * Mathf.Sin(Mathf.PI * Time.timeSinceLevelLoad);
+        rend.material.SetColor("_TintColor", currentColor);
+        // GetComponent<ParticleSystemRenderer>().material.color = Color.Lerp(Color.red, Color.blue, 0.5f);
+    }
+}
