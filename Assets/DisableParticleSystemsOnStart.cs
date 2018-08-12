6d5b5c08fe9ae7de01a7b0231cee16edec68ae8d
Unknown <yossi@currentcircus.com>
2018-08-12 14:53:48 +0300

    WIP commit towards using multiple timelines

---
+++ Assets/DisableParticleSystemsOnStart.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableParticleSystemsOnStart : MonoBehaviour
+{
+
+    public bool DisableOnStart = true;
+
+    public ParticleSystem[] ParticleSystems;
+
+	void Start ()
+    {
+        if (ParticleSystems == null || ParticleSystems.Length == 0)
+            ParticleSystems = GetComponentsInChildren<ParticleSystem>(true);
+
+		if (ParticleSystems != null)
+        {
+            for(int i=0; i<ParticleSystems.Length; i++)
+            {
+                var emission = ParticleSystems[i].emission;
+                emission.enabled = false;
+                //ParticleSystem.MainModule emit = ParticleSystems[i].main;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
